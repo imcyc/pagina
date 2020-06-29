b205b763913a1f36756083c226d95eb8c3b83376
Axel Obscura <axosar@gmail.com>
2020-06-29 12:34:53 -0500

    Header

--- src/components/Header/HeaderConfig.js
+++ src/components/Header/HeaderConfig.js
@@ -14,9 +14,10 @@ function HeaderConfig(props) {
 
     return (
         <div className="header-action mt-lg-3 text-right">
-            <a href="tel:00199823568658" className="tel-no">+998 23568 658</a>
-            <button onClick={LoginRegHandler} className="btn-cog"><i className="fa fa-cog"/></button>
-            <button onClick={MobileMenuHandler} className="btn-menu d-lg-none"><i className="fa fa-bars"/></button>
+            <a href="tel:5553225740" className="tel-no">+(55) 5322 5740</a>
+            <a href="mail_to:imcyc@imcyc.com" className="tel-no">imcyc@imcyc.com</a>
+            <button onClick={LoginRegHandler} className="btn-cog"><i className="fa fa-cog" /></button>
+            <button onClick={MobileMenuHandler} className="btn-menu d-lg-none"><i className="fa fa-bars" /></button>
         </div>
     );
 }
