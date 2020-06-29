9d899fb50b29208787a05fa7cfcc03dba071511d
Axel Obscura <axosar@gmail.com>
2020-06-29 09:37:17 -0500

    Updates

---
+++ precache-manifest.d38288e75b42fdfe9811ed356a4771bc.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140bfac8d390f940688643dc29e37a1b",
+    "url": "/pagina/index.html"
+  },
+  {
+    "revision": "39fe24b9a899de16360d",
+    "url": "/pagina/static/css/2.b3667e1f.chunk.css"
+  },
+  {
+    "revision": "36763670af4d56327961",
+    "url": "/pagina/static/css/main.6ecf31b2.chunk.css"
+  },
+  {
+    "revision": "39fe24b9a899de16360d",
+    "url": "/pagina/static/js/2.3c213a47.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pagina/static/js/2.3c213a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36763670af4d56327961",
+    "url": "/pagina/static/js/main.19cea4e7.chunk.js"
+  },
+  {
+    "revision": "e021eb876778d8acdb18",
+    "url": "/pagina/static/js/runtime-main.d2174b21.js"
+  },
+  {
+    "revision": "269c71f648fdc17b728c0e7cfc6f98fc",
+    "url": "/pagina/static/media/01.269c71f6.jpg"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/pagina/static/media/01.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/01.99492839.jpg"
+  },
+  {
+    "revision": "ab7662de93df4d8b97561ea50f853703",
+    "url": "/pagina/static/media/01.ab7662de.jpg"
+  },
+  {
+    "revision": "269c71f648fdc17b728c0e7cfc6f98fc",
+    "url": "/pagina/static/media/02.269c71f6.jpg"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/pagina/static/media/02.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/02.99492839.jpg"
+  },
+  {
+    "revision": "ab7662de93df4d8b97561ea50f853703",
+    "url": "/pagina/static/media/02.ab7662de.jpg"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/pagina/static/media/03.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/03.99492839.jpg"
+  },
+  {
+    "revision": "ab7662de93df4d8b97561ea50f853703",
+    "url": "/pagina/static/media/03.ab7662de.jpg"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/pagina/static/media/04.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/04.99492839.jpg"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/pagina/static/media/05.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/05.99492839.jpg"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/pagina/static/media/06.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/06.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/07.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/08.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/09.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/10.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/11.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/12.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/13.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/14.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/pagina/static/media/15.99492839.jpg"
+  },
+  {
+    "revision": "829d452c64c4b55779e2140db2c59edb",
+    "url": "/pagina/static/media/404.829d452c.png"
+  },
+  {
+    "revision": "011111bdd037740918516ee25bafecfc",
+    "url": "/pagina/static/media/about-2-bg.011111bd.jpg"
+  },
+  {
+    "revision": "e5ae11aaf60884c8efaf836097345fd2",
+    "url": "/pagina/static/media/about-2.e5ae11aa.jpg"
+  },
+  {
+    "revision": "17a1b230bf29246af179e1df091f41ab",
+    "url": "/pagina/static/media/blog-r-g.17a1b230.jpg"
+  },
+  {
+    "revision": "d006437becb84e03525bf006190b2a32",
+    "url": "/pagina/static/media/favicon.d006437b.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pagina/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pagina/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pagina/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pagina/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pagina/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "34c6010a2a87fcd287f2680335bc516a",
+    "url": "/pagina/static/media/fun-fact-bg.34c6010a.jpg"
+  },
+  {
+    "revision": "269c71f648fdc17b728c0e7cfc6f98fc",
+    "url": "/pagina/static/media/h-2-01.269c71f6.jpg"
+  },
+  {
+    "revision": "bbc1807e3f534526e9f1d92823aca46d",
+    "url": "/pagina/static/media/h-2-02.bbc1807e.jpg"
+  },
+  {
+    "revision": "118275c1093bba01cee6c2c54b3e7193",
+    "url": "/pagina/static/media/page-header.118275c1.jpg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/pagina/static/media/react.05a3e30f.svg"
+  },
+  {
+    "revision": "1a0ae75dcd69a632f8c9cef7e03babf1",
+    "url": "/pagina/static/media/service-bg.1a0ae75d.jpg"
+  },
+  {
+    "revision": "aca54e4d62bf00e7ef808e315e33ff03",
+    "url": "/pagina/static/media/slider-bg.aca54e4d.jpg"
+  },
+  {
+    "revision": "5ed35a47e7bc6ab6952239ef11841642",
+    "url": "/pagina/static/media/team-bg-2.5ed35a47.jpg"
+  },
+  {
+    "revision": "efdc4e1a3d8b94a3c195efe58f743069",
+    "url": "/pagina/static/media/team-bg.efdc4e1a.jpg"
+  },
+  {
+    "revision": "3f284518090f05a859af1e6418a56978",
+    "url": "/pagina/static/media/team-details.3f284518.jpg"
+  },
+  {
+    "revision": "309edc077941230611f1dd502ef2d38b",
+    "url": "/pagina/static/media/team.309edc07.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pagina/precache-manifest.d38288e75b42fdfe9811ed356a4771bc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pagina/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
